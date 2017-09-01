b14759b799a23dd842eb03f32c8d620eb2af670e
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-01 13:47:25 +0700

    Finished

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+<script>
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
+<script>
+        $("#carousel-button").click(function () {
+            if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+        $("#carousel-button").children("span").removeClass('fa-pause');
+                $("#carousel-button").children("span").addClass('fa-play');
+            }
+            else if ($("#carousel-button").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+        $("#carousel-button").children("span").removeClass('fa-play');
+                $("#carousel-button").children("span").addClass('fa-pause');
+            }
+        });
+</script>
